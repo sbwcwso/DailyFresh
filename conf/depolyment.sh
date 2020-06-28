fbce2b3e938a5f9b502574ce168267fb9bf6ba4b
Lijunjie9502 <lijunjie199502@gmail.com>
2020-06-28 08:21:44 +0800

    添加配置项

---
+++ conf/depolyment.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 启动fastdfs
+sudo service fdfs_trackerd start
+sudo service fdfs_storaged start
+
+# 启动nginx 服务
+sudo /usr/local/nginx/sbin/nginx -c /home/lijunjie/Documents/Django/DailyFresh/conf/nginx/nginx.conf
+
+# 启动　redis
+sudo redis-server /etc/redis/redis.conf
+
+# 启动 celery
+/home/lijunjie/.virtualenvs/DailyFresh/bin/celery --workdir="/home/lijunjie/Documents/Django/DailyFresh_Celery/dailyfresh" -A celery_task.tasks worker -l info > /tmp/celery.log 2>&1 &
+
+# 启动 uwsgi
+/home/lijunjie/.virtualenvs/DailyFresh/bin/uwsgi --ini /home/lijunjie/Documents/Django/DailyFresh/conf/uwsgi/uwsgi.ini
